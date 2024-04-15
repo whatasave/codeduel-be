03f607bb108979cfc0280d86f00d971f06465fa6
Kaj Rusilowski <xedom08@gmail.com>
2024-04-15 21:59:33 +0200

    Update error handling and logging in main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
   // loading env only if not in production
   if utils.GetEnv("ENV", "development") == "development" {
     if err := godotenv.Load(); err != nil {
-      log.Println("%s Error loading .env file", utils.GetLogTag("main"))
+      log.Printf("%s Error loading .env file", utils.GetLogTag("main"))
     }
   }
   config := config.LoadConfig()
@@ -27,10 +27,10 @@ func main() {
     config.MariaDBPassword,
     config.MariaDBDatabase,
   )
-  if err != nil { log.Printf("%s Error creating DB instance: %v", utils.GetLogTag("main"), err) }
+  if err != nil { log.Printf("%s Error creating DB instance: %v", utils.GetLogTag("DB"), err.Error() ) }
   defer db.Close()
-  if err := db.InitUserTables(); err != nil { log.Printf("%s Error initializing DB user tables: %v", utils.GetLogTag("main"), err) }
-  if err := db.InitMatchTables(); err != nil { log.Printf("%s Error initializing DB match tables: %v", utils.GetLogTag("main"), err) }
+  if err := db.InitUserTables(); err != nil { log.Printf("%s Error initializing DB user tables: %v", utils.GetLogTag("DB"), err.Error()) }
+  if err := db.InitMatchTables(); err != nil { log.Printf("%s Error initializing DB match tables: %v", utils.GetLogTag("DB"), err.Error()) }
 
   server := api.NewAPIServer(config, db)
   server.Run()
